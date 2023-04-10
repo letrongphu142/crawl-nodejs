d3666dd2a45fab194a32e7a44d732e0fa3fd2f9f
Letrongphu <Letrongphu142@gmail.com>
2023-04-10 18:43:33 +0700

    update crawlData Pokemon

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const cheerio = require('cheerio'); // khai báo module cheerio
 const fs = require('fs'); // require thêm module filesystem
 const request = require('request-promise'); // khai báo module request-promise
-const logger = require('morgan'); //khai báo dependence morgan
-const express = require('express'); //khai báo dependence express
+const axios = require("axios");
 
 //Hàm gọi các page để get data
 const crawlDataAllPage = async (url) => {
     return await request(url).then(response => new Promise(resolve => { // gửi request đến trang
-        console.log('log', typeof response)
         if (response) {
             const $ = cheerio.load(response);
             let data = []
@@ -33,7 +31,9 @@ const handelCall = async (index = 1) => {
     if (index == 8) {
         fs.writeFileSync('data.json', JSON.stringify(fullData)); // lưu dữ liệu vào file data.json
     } else {
-        const crawlData = await crawlDataAllPage(`https://123job.vn/tuyen-dung?sort=new&page=${index}`);
+        //khai bao url page
+        const url = `https://123job.vn/tuyen-dung?sort=new&page=${index}`;
+        const crawlData = await crawlDataAllPage(url);
         if (crawlData && crawlData.data) {
             fullData = fullData.concat(crawlData.data);
             handelCall(index + 1);
@@ -45,4 +45,52 @@ const handelCall = async (index = 1) => {
         }
     }
 };
+//=============================================================================
+//Crawl data website Pokemon
+const crawlDataPokemon = async (maxPages = 50) => {
+    // khai bao url page
+    const paginationURLsToVisit = ["https://scrapeme.live/shop"];
+    const visitedURLs = [];
+
+    const productURLs = new Set();
+
+    while (
+        paginationURLsToVisit.length !== 0 &&
+        visitedURLs.length <= maxPages
+    ) {
+        // trang dang crawl
+        const paginationURL = paginationURLsToVisit.pop();
+
+        // Duyet HTML
+        const pageHTML = await axios.get(paginationURL);
+
+        //xac nhan da crwal
+        visitedURLs.push(paginationURL);
+
+        // Crawl data vs cheerio
+        const $ = cheerio.load(pageHTML.data);
+
+        // Duyet
+        $(".page-numbers a").each((index, element) => {
+            const paginationURL = $(element).attr("href");
+            if (
+                !visitedURLs.includes(paginationURL) &&
+                !paginationURLsToVisit.includes(paginationURL)
+            ) {
+                paginationURLsToVisit.push(paginationURL);
+            }
+        });
+
+        $("li.product a.woocommerce-LoopProduct-link").each((index, element) => {
+            const productURL = $(element).attr("href");
+            productURLs.add(productURL);
+        });
+    }
+
+    // Luu du lieu vao pokemon.json
+    fs.writeFileSync('pokemon.json', JSON.stringify([...productURLs]));
+}
+//================================================================================
 handelCall();
+
+crawlDataPokemon();
